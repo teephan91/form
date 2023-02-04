c73acdcaf9986da61ed9f069934fd5b446db5621
Thinh Phan <pseudothinhphan@gmail.com>
2023-02-03 18:52:40 -0800

    Trim down script file to prevent repeating codes
    Doing this also changes codes in various places in the
    index.html & style.css files.

--- js/form.js
+++ js/form.js
@@ -1,21 +1,21 @@
-const userName = document.getElementById('name');
-const userNameLabel = document.querySelector('.username span');
-
-const userEmail = document.getElementById('email');
-const userEmailLabel = document.querySelector('.useremail span')
-
 const userPwd = document.getElementById('password');
-const userPwdLabel = document.querySelector('.userpwd span');
-const theEye = document.querySelector('.eye img');
-
-const userCoName = document.getElementById('company_name');
-const userCoNameLabel = document.querySelector('.userconame span');
-
-const userURL = document.getElementById('proofhub_url');
-const userURLLabel = document.querySelector('.userurl span');
-
+const theEye = document.querySelector('.eye');
 const allInputs = Array.from(document.querySelectorAll('input'));
 const continueBtn = document.querySelector('.continue_btn');
+
+// Event listener for all inputs
+allInputs.forEach((input) => {
+    input.addEventListener('input', () => {
+        if (input.validity.valid) {
+            input.previousElementSibling.lastChild.textContent = "";
+            input.classList.remove('error_input');
+            checkValidity();
+        } else {
+            showError(input);
+            checkValidity();
+        }
+    });
+});
 
 // For checking validity of all inputs before enabling Continue/Submit button
 function isValid() {
@@ -32,42 +32,7 @@ function checkValidity() {
 }
 }
 
-// Name input
-userName.addEventListener('input', () => {
-    if (userName.validity.valid) {
-        userNameLabel.textContent = "";
-        userName.classList.remove('error_input');
-        checkValidity();
-    } else {
-        showError(userName);
-        checkValidity();
-    }
-});
-
-// Email input
-userEmail.addEventListener('input', () => {
-    if (userEmail.validity.valid) {
-        userEmailLabel.textContent = "";
-        userEmail.classList.remove('error_input');
-        checkValidity();
-    } else {
-        showError(userEmail);
-        checkValidity();
-    }
-});
-
-// Password input
-userPwd.addEventListener('input', () => {
-    if (userPwd.validity.valid) {
-        userPwdLabel.textContent = "";
-        userPwd.classList.remove('error_input');
-        checkValidity();
-    } else {
-        showError(userPwd);
-        checkValidity();
-    }
-});
-
+// For the hide/show eye
 let a;
 function showPwd() {
     if (a == 1) {
@@ -83,63 +48,39 @@ function showPwd() {
     }
 }
 
-// Company name input
-userCoName.addEventListener('input', () => {
-    if (userCoName.validity.valid) {
-        userCoNameLabel.textContent = "";
-        userCoName.classList.remove('error_input');
-        checkValidity();
-    } else {
-        showError(userCoName);
-        checkValidity();
-    }
-});
-
-// ProofHub URL input
-userURL.addEventListener('input', () => {
-    if (userURL.validity.valid) {
-        userURLLabel.textContent = "";
-        userURL.classList.remove('error_input');
-        checkValidity();
-    } else {
-        showError(userURL);
-        checkValidity();
-    }
-});
-
-// For all inputs
+// Show different error messages for different inputs
 function showError(input) {
-    if (input == userName) {
-        if (userName.validity.valueMissing) {
-            userNameLabel.textContent = "Hi! I'm Bot, James Bot. And you're?";
-        } else if (userName.validity.patternMismatch) {
-            userNameLabel.textContent = "Special characters are not allowed!";
+    if (input.id === 'name') {
+        if (input.validity.valueMissing) {
+            input.previousElementSibling.lastChild.textContent = "Hi! I'm Bot, James Bot. And you're?";
+        } else if (input.validity.patternMismatch) {
+            input.previousElementSibling.lastChild.textContent = "Special characters are not allowed!";
         } 
-    } else if (input == userEmail) {
-        if (userEmail.validity.valueMissing) {
-            userEmailLabel.textContent = "This field is required.";
-        } else if (userEmail.validity.typeMismatch) {
-            userEmailLabel.textContent = "Please enter a valid email address.";
+    } else if (input.id === 'email') {
+        if (input.validity.valueMissing) {
+            input.previousElementSibling.lastChild.textContent = "This field is required.";
+        } else if (input.validity.typeMismatch) {
+            input.previousElementSibling.lastChild.textContent = "Please enter a valid email address.";
         }
-    } else if (input == userPwd) {
-        if (userPwd.validity.valueMissing) {
-            userPwdLabel.textContent = "This field is required.";
-        } else if (userPwd.validity.tooShort) {
-            userPwdLabel.textContent = "Please enter at least 5 characters.";
+    } else if (input.id === 'password') {
+        if (input.validity.valueMissing) {
+            input.previousElementSibling.lastChild.textContent = "This field is required.";
+        } else if (input.validity.tooShort) {
+            input.previousElementSibling.lastChild.textContent = "Please enter at least 5 characters.";
         }
-    } else if (input == userCoName) {
-        if (userCoName.validity.valueMissing) {
-            userCoNameLabel.textContent = "This field is required.";
-        } else if (userCoName.validity.patternMismatch) {
-            userCoNameLabel.textContent = "Special characters are not allowed!";
+    } else if (input.id === 'company_name') {
+        if (input.validity.valueMissing) {
+            input.previousElementSibling.lastChild.textContent = "This field is required.";
+        } else if (input.validity.patternMismatch) {
+            input.previousElementSibling.lastChild.textContent = "Special characters are not allowed!";
         }
-    } else if (input == userURL) {
-        if (userURL.validity.valueMissing) {
-            userURLLabel.textContent = "This field is required.";
-        } else if (userURL.validity.patternMismatch) {
-            userURLLabel.textContent = "Only letters, numbers or hyphens are allowed.";
-        } else if (userURL.validity.tooShort) {
-            userURLLabel.textContent = "Please enter at least 3 characters."
+    } else if (input.id === 'proofhub_url') {
+        if (input.validity.valueMissing) {
+            input.previousElementSibling.lastChild.textContent = "This field is required.";
+        } else if (input.validity.patternMismatch) {
+            input.previousElementSibling.lastChild.textContent = "Only letters, numbers or hyphens are allowed.";
+        } else if (input.validity.tooShort) {
+            input.previousElementSibling.lastChild.textContent = "Please enter at least 3 characters."
         }
     }
     input.className = "error_input";
